4b4a662782712ed5a19448b5f2343920c73fab2d
Rakib Amin <md.rakib.amin@gmail.com>
2020-07-12 11:00:52 +0800

    first commit to repo

---
+++ src/google/TestSolutions.java
@@ -0,0 +1,15 @@
+package google;
+
+import java.util.Scanner;
+
+public class TestSolutions {
+	
+	public static void main(String[] a) {
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNext()) {
+			String s = sc.next();
+			System.out.print(s);
+		}
+	}
+
+}
